77df83a6b7d7f719e2a5332b745ad9c0a61247ae
Anonim <nelubitelzmej@gmail.com>
2024-09-19 17:29:45 -0700

    changed footer

--- src/components/Layout/Sections/Footer/Footer.jsx
+++ src/components/Layout/Sections/Footer/Footer.jsx
@@ -84,10 +84,7 @@ const contactsLinks = [
 
 const Footer = () => {
   return (
-    <footer className='footer1
-    
-    
-    '>
+    <footer className='footer'>
       <a href="#hero" className='footer__site-logo--wrapper'>
         <SiteLogo classNames='footer__site-logo' />
       </a>
